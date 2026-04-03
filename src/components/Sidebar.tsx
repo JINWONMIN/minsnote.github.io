1f2bf3ef668835ac5a5edc0148fbb5dd9826f2d5
jinwonmin <minjw02@gmail.com>
2026-04-03 17:44:59 +0900

    Style sidebar scrollbar to match dark theme

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,10 +7,10 @@ interface SidebarProps {
 export default function Sidebar({ children }: SidebarProps) {
   return (
     <aside className="hidden lg:flex lg:flex-col w-60 shrink-0 border-r border-gray-200 dark:border-gray-800">
-      <div className="sticky top-16 flex flex-col h-[calc(100vh-4rem)] overflow-y-auto">
+      <div className="sticky top-16 flex flex-col h-[calc(100vh-4rem)]">
         <ProfileCard />
         <div className="border-t border-gray-200 dark:border-gray-800" />
-        <div className="flex-1 overflow-y-auto p-4">
+        <div className="flex-1 overflow-y-auto sidebar-scroll p-4">
           {children}
         </div>
       </div>
